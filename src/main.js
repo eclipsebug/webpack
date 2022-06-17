5738b9d2e85199d91fc0d63495ff86bb42552b0a
eclipsebug <860337520@qq.com>
2022-06-17 21:00:17 +0800

    完成图片

--- src/main.js
+++ src/main.js
@@ -6,3 +6,25 @@ import './tabs.js'
 
 import './styles/index.css'
 import './styles/index.less'
+
+// 1.引入图片
+import gifSrc from './assets/1.gif'
+import pngSrc from './assets/logo_small.png'
+
+// 2. 创建图片节点
+const gif = document.createElement('img')
+const png = document.createElement('img')
+
+// 3. 给src赋值
+gif.src = gifSrc
+png.src = pngSrc
+
+// 4. 插入节点
+document.body.appendChild(gif)
+document.body.appendChild(png)
+import './assets/fonts/iconfont.css'
+
+const fn = () =>
+    console.log("你好");
+
+console.log(fn);
--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,21 @@ module.exports = {
                 test: /\.less$/,
                 // 使用less-loader, 让webpack处理less文件, 内置还会用less翻译less代码成css内容
                 use: ["style-loader", "css-loader", 'less-loader']
+            },
+            {
+                test: /\.(png|jpg|gif|jpeg)$/i,
+                use: [
+                    {
+                        loader: 'url-loader', // 匹配文件, 尝试转base64字符串打包到js中
+                        // 配置limit, 超过8k, 不转, file-loader复制, 随机名, 输出文件
+                        options: {
+                            limit: 2 * 1024,
+                            name: '[name].[ext]',
+                            outputPath: " lib/images/",
+                            clean: true
+                        },
+                    },
+                ],
             }
         ]
     }
