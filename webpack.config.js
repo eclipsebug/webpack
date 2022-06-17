4239802526382d2381cb43478b228bd74b6be089
eclipsebug <860337520@qq.com>
2022-06-17 19:40:23 +0800

    完成了html

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 const path = require('path')
-
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
     entry: "./src/main.js",
     output: {
         path: path.join(__dirname, 'lib'),
-        filename: "webpack-demo.js",
+        filename: "index.js",
         clean: true
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin
+    ]
 }
